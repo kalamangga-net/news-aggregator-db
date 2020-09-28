7e8a3469c10e54e3f98405ad4293f7e348807a22
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2020-09-29 01:30:23 +0700

    move to aria and prep for yii2 based viewer

--- db-structure/db-structure.sql
+++ db-structure/db-structure.sql
@@ -1,27 +1,41 @@
 CREATE DATABASE IF NOT EXISTS `kalamangga`;
 USE `kalamangga`;
 
+DROP TABLE IF EXISTS `kategori`;
+CREATE TABLE `kategori` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kategori` varchar(30) NOT NULL,
+  `label` varchar(35) NOT NULL,
+  `keterangan` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `kategori` (`kategori`)
+) ENGINE=Aria AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 PAGE_CHECKSUM=1;
+
 DROP TABLE IF EXISTS `link`;
 CREATE TABLE `link` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_sindikasi` tinyint(4) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
   `judul` tinytext NOT NULL,
   `link` varchar(300) NOT NULL,
-  `tanggal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp(),
   `ringkasan` text NOT NULL,
+  `md5` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `link` (`link`),
+  UNIQUE KEY `md5` (`md5`),
   KEY `id_sindikasi` (`id_sindikasi`),
+  KEY `kategori` (`id_kategori`),
   FULLTEXT KEY `judul` (`judul`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+) ENGINE=Aria AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 PAGE_CHECKSUM=0 TRANSACTIONAL=0;
 
 DROP TABLE IF EXISTS `menu`;
 CREATE TABLE `menu` (
   `id_menu` int(11) NOT NULL AUTO_INCREMENT,
-  `nm_menu` varchar(35) NOT NULL,
-  `link` varchar(100) NOT NULL,
+  `label` varchar(35) NOT NULL,
+  `url` varchar(100) NOT NULL,
   PRIMARY KEY (`id_menu`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=Aria AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 PAGE_CHECKSUM=0 TRANSACTIONAL=0;
 
 DROP TABLE IF EXISTS `xml`;
 CREATE TABLE `xml` (
@@ -31,6 +45,6 @@ CREATE TABLE `xml` (
   `judul` varchar(75) NOT NULL,
   `link` varchar(100) NOT NULL,
   `img` varchar(100) NOT NULL,
-  `fetch` tinyint(1) NOT NULL DEFAULT '1',
+  `fetch` tinyint(1) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=Aria AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 PAGE_CHECKSUM=0 TRANSACTIONAL=0;
